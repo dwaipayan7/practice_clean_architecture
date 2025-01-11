7b2b6d5908bf9bd65020a710f0f9ed341e5d905a
Dwaipayan Biswas <biswastatay73@gmail.com>
2025-01-11 23:49:44 +0530

    Initial commit

---
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
---
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatefulWidget {
+  static route() => MaterialPageRoute(builder: (_) => SignupPage(),);
+  const SignupPage({super.key});
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final nameController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    super.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    nameController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
