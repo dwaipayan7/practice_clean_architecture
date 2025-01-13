09111d8cc00434620f0055125580a4a77d7ac35c
Dwaipayan Biswas <biswastatay73@gmail.com>
2025-01-13 22:06:20 +0530

    updated

---
+++ lib/features/auth/data/repository/auth_repository_impl.dart
@@ -0,0 +1,40 @@
+import 'package:fpdart/src/either.dart';
+import 'package:practice_clean_architecture/cors/error/exception.dart';
+import 'package:practice_clean_architecture/cors/error/failure_dart.dart';
+import 'package:practice_clean_architecture/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:practice_clean_architecture/features/auth/domain/repository/auth_repository.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  final AuthRemoteDataSource remoteDataSource;
+
+  AuthRepositoryImpl({required this.remoteDataSource});
+
+  @override
+  Future<Either<Failure, String>> loginWithEmailPassword({
+    required String email,
+    required String password,
+  }) {
+    // TODO: implement loginWithEmailPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, String>> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+    final userId = await remoteDataSource.signUpWithEmailPassword(
+        name: name,
+        email: email,
+        password: password,
+      );
+
+    return Either.right(userId);
+
+    } on ServerException catch (e) {
+     return Either.left(Failure(e.message));
+    }
+  }
+}
